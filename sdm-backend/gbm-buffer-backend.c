ed0e261061b47eeab39ecac6b233d8d93059aa3b
Xiaoyu Huang <quic_xiahua@quicinc.com>
2022-07-01 11:50:00 +0800

    Fix compiler Warnings of -Wincompatible-pointer-types
    Minor fixes to prevent assignments/argument passing from
    incompatible pointer type. No behavior change.
    
    Change-Id: I3a10c3b80b5cd297fc6eb7d1681a1bab0d80bbef
    Signed-off-by: Xiaoyu Huang <quic_xiahua@quicinc.com>

--- sdm-backend/gbm-buffer-backend.c
+++ sdm-backend/gbm-buffer-backend.c
@@ -81,7 +81,7 @@ gbm_buffer_backend_create_buffer(struct wl_client *client,
 		uint32_t width,
 		uint32_t height,
 		uint32_t format,
-		uint32_t flags);
+		int32_t flags);
 
 static void
 gbm_buffer_backend_destroy(struct wl_client *client,
@@ -264,11 +264,10 @@ gbm_buffer_backend_create_buffer(struct wl_client *client,
 		uint32_t width,
 		uint32_t height,
 		uint32_t format,
-		uint32_t flags)
+		int32_t flags)
 {
 	struct gbm_buffer *buffer;
 	bool ret = true;
-
 
 	GBM_PROTOCOL_LOG(LOG_DBG,"gbm_buffer_backend_create_buffer::Invoked\n");
 
