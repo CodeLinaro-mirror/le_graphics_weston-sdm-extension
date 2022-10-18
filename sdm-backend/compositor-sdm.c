a82d1f0e12200e79f272a2f14394a90d4ec00b37
Xiaoyu Huang <quic_xiahua@quicinc.com>
2022-10-18 18:02:30 +0800

    sdm-backend: replace virtual output with original output
    Use the original output instead of virtual output to do capture_screen.
    
    Change-Id: I0a37640ecc075af798d35ed74b16c3f31d741d5a
    Signed-off-by: Xiaoyu Huang <quic_xiahua@quicinc.com>

--- sdm-backend/compositor-sdm.c
+++ sdm-backend/compositor-sdm.c
@@ -857,7 +857,6 @@ do_screen_capture(struct screen_capture *screen_cap,
 		pixman_region32_t *damage)
 {
 	struct screen_capture_buffer *cap_buf = screen_cap->next;
-	struct drm_output *virt_output = screen_cap->virtual_output;
 
 	/*
 	 * Decrease the attached refcnt after increasing composition refcnt to
@@ -867,8 +866,8 @@ do_screen_capture(struct screen_capture *screen_cap,
 	weston_buffer_reference(&cap_buf->buf_ref, NULL);
 
 	if (screen_cap->fallback_gpu) {
-		screen_cap->compositor->renderer->capture_screen(&virt_output->base,
-					cap_buf->buffer, damage, screen_cap->mirror_output);
+		screen_cap->compositor->renderer->capture_screen(screen_cap->mirror_output,
+						cap_buf->buffer, damage);
 
 		/* Release the buffer once GPU composition is completed */
 		weston_buffer_reference(&screen_cap->buf_ref, NULL);
