c51b7c9db2ef2d0e1734c78d32e9a309e28ee7e2
Xiaoyu Huang <xiahua@qti.qualcomm.com>
2024-05-14 13:30:56 +0800

    sdm-service: set teardown true in power off
    Change-Id: I8b986146997067f361e0787dc386d08c7b4ef622
    Signed-off-by: Lan Zou <quic_lanz@quicinc.com>

--- sdm-service/sdm_display.cpp
+++ sdm-service/sdm_display.cpp
@@ -264,11 +264,12 @@ DisplayError SdmDisplay::HandleEvent(DisplayEvent event) {
 
 DisplayError SdmDisplay::SetDisplayState(DisplayState state) {
   DisplayError error;
-  int release_fence = -1;
+  bool teardown = false;
 
-  error = display_intf_->SetDisplayState(state, false /* teardown */, &release_fence);
-  if (release_fence >= 0)
-    close(release_fence);
+  if (state == kStateOff)
+    teardown = true;
+
+  error = display_intf_->SetDisplayState(state, teardown, nullptr);
 
   if (error != kErrorNone) {
     DLOGE("function failed. Error = %d", error);
