c8f9e929adeec92c3873fb68136eb9e53f44f938
ruoyyang <quic_ruoyyang@quicinc.com>
2024-10-30 19:53:01 +0800

    sdm-backend: add module to handle deep sleep with snservice
    Add the compositor-pm-ds-snservice to react to DS processes.
    
    Change-Id: Id9444f5b71ca062e6b1f080b1b232bdba840ea8b
    Signed-off-by: ruoyyang <quic_ruoyyang@quicinc.com>
    (cherry picked from commit 10048d64434a3650e4ec1d1bbf32ccb49d63b8b4)

---
+++ sdm-backend/compositor-pm-ds-snservice.c
@@ -0,0 +1,203 @@
+/*
+*    This file provide a module to deal with deep sleep.
+*    Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+*    SPDX-License-Identifier: BSD-3-Clause-Clear
+*/
+
+#include <stdlib.h>
+#include <poll.h>
+#include <sys/eventfd.h>
+#include <pm_client_lib.h>
+#include <wayland-server.h>
+#include <libweston/zalloc.h>
+#include <libweston/libweston.h>
+#include "weston-shared/helpers.h"
+
+#ifndef EOK
+#define EOK 0
+#endif
+
+#ifndef E_NOK
+#define E_NOK -1
+#endif
+
+#define PM_DISPLAY_CLIENT_NAME "pm-client-weston"
+
+enum {
+	EVENT_SUSPEND = 1,
+	EVENT_RESUME  = 2,
+	EVENT_MAX,
+};
+
+struct snservice_notifier {
+	struct wl_event_source *snservice_source;
+	struct wl_listener compositor_destroy_listener;
+	struct weston_compositor *compositor;
+	int pm_ev_fd;
+	struct pm_ops_s ops;
+	pm_client_t pm_handle;
+};
+
+struct snservice_notifier snservice_notifier;
+
+static int
+weston_compositor_ds_suspend(void *data, enum PM_MODE mode)
+{
+	struct snservice_notifier *notifier = (struct snservice_notifier *)data;
+	uint64_t u = EVENT_SUSPEND;
+
+	if (!notifier) {
+		weston_log("SN Suspend: notifier null \n");
+		return E_NOK;
+	}
+
+	weston_log("SN Suspend: Trigger suspend event \n");
+	write(notifier->pm_ev_fd, &u, sizeof(uint64_t));
+
+	return EOK;
+}
+
+static int
+weston_compositor_ds_resume(void *data, enum PM_MODE mode)
+{
+	struct snservice_notifier *notifier = (struct snservice_notifier *)data;
+	uint64_t u = EVENT_RESUME;
+
+	if (!notifier) {
+		weston_log("SN Resume: notifier null \n");
+		return E_NOK;
+	}
+
+	weston_log("SN Resume: Trigger resume event \n");
+	write(notifier->pm_ev_fd, &u, sizeof(uint64_t));
+
+	return EOK;
+}
+
+static void
+snservice_ds_handler(int fd, uint32_t mask, void* data)
+{
+	struct snservice_notifier *notifier = data;
+	int ret = EOK;
+	uint64_t event_type;
+
+	read(fd, &event_type, sizeof(uint64_t));
+	switch (event_type)
+	{
+		case EVENT_SUSPEND:
+			weston_compositor_sleep(notifier->compositor);
+			weston_log("SN Suspend: weston sleep \n ");
+			break;
+
+		case EVENT_RESUME:
+			weston_compositor_wake(notifier->compositor);
+			weston_log("SN Resume: weston wakeup \n");
+			break;
+		default:
+			weston_log("SN: Unknown pm event type \n");
+			break;
+	}
+}
+
+static int
+weston_compositor_enable_ds_event(struct snservice_notifier *notifier)
+{
+	struct wl_event_loop *loop;
+
+	notifier->pm_ev_fd = eventfd(0, EFD_CLOEXEC | EFD_NONBLOCK);
+	if (notifier->pm_ev_fd < 0)
+		return E_NOK;
+
+	loop = wl_display_get_event_loop(notifier->compositor->wl_display);
+	if (!loop) {
+		weston_log("SN: no loop get \n");
+		goto err_get_loop;
+	}
+
+	notifier->snservice_source =
+			wl_event_loop_add_fd(loop, notifier->pm_ev_fd,
+					WL_EVENT_READABLE, snservice_ds_handler, notifier);
+
+	return EOK;
+
+err_get_loop:
+	close(notifier->pm_ev_fd);
+	notifier->pm_ev_fd = -1;
+
+	return E_NOK;
+}
+
+static void
+weston_compositor_disable_ds_event(struct snservice_notifier *notifier)
+{
+	if (notifier->snservice_source) {
+		wl_event_source_remove(notifier->snservice_source);
+		notifier->snservice_source = NULL;
+	}
+
+	if (notifier->pm_ev_fd) {
+		close(notifier->pm_ev_fd);
+		notifier->pm_ev_fd = -1;
+	}
+}
+
+static void
+weston_compositor_destroy_snservice_listener(struct wl_listener *listener, void *data)
+{
+	struct snservice_notifier *notifier;
+
+	notifier = container_of(listener, struct snservice_notifier, compositor_destroy_listener);
+
+	if (notifier->pm_handle)
+		pm_deregister(notifier->pm_handle);
+
+	weston_compositor_disable_ds_event(notifier);
+	wl_list_remove(&notifier->compositor_destroy_listener.link);
+	free(notifier);
+}
+
+WL_EXPORT int
+wet_module_init(struct weston_compositor *compositor, int *argc, char *argv[])
+{
+	int32_t ret = EOK;
+
+	if (!compositor)
+		return E_NOK;
+
+	if (!weston_compositor_add_destroy_listener_once(compositor,
+			&snservice_notifier.compositor_destroy_listener,
+			weston_compositor_destroy_snservice_listener)) {
+		weston_log("SN: weston_compositor_add_destroy_listener_once fail \n");
+		goto err_out;
+	}
+
+	snservice_notifier.compositor = compositor;
+	snservice_notifier.ops.pm_enter = weston_compositor_ds_suspend;
+	snservice_notifier.ops.pm_exit = weston_compositor_ds_resume;
+
+	ret = weston_compositor_enable_ds_event(&snservice_notifier);
+	if (ret) {
+		weston_log("SN: weston_compositor_enable_ds_event fail \n");
+		goto err_event;
+	}
+
+	ret = pm_register((char *)PM_DISPLAY_CLIENT_NAME,
+			&snservice_notifier.ops,
+			(void*)&snservice_notifier,
+			&snservice_notifier.pm_handle);
+	if (ret) {
+		weston_log("SN: pm_register fail, ret %d\n", ret);
+		goto err_register;
+	}
+
+	weston_log("SN: register snserivce ops for client %s\n", PM_DISPLAY_CLIENT_NAME);
+
+	return ret;
+
+err_register:
+	weston_compositor_disable_ds_event(&snservice_notifier);
+err_event:
+	wl_list_remove(&snservice_notifier.compositor_destroy_listener.link);
+err_out:
+	return E_NOK;
+}
