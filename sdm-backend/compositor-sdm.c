3845c72b8c8c740e4dbd8fcea31f075c7c06f547
miche <quic_miche@quicinc.com>
2025-01-10 17:28:11 +0800

    sdm-backend: refine thread scheduling based on compresmgr library
    Create thread using the value of compresmgr library if successfully, otherwise use the default value.
    
    Change-Id: I1e832a3c9bb5d389ee45617e64412cdd3c12213e
    Signed-off-by: miche <quic_miche@quicinc.com>

--- sdm-backend/compositor-sdm.c
+++ sdm-backend/compositor-sdm.c
@@ -48,7 +48,7 @@
 * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 *
 * Changes from Qualcomm Innovation Center are provided under the following license:
-* Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+* Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
 * SPDX-License-Identifier: BSD-3-Clause-Clear
 */
 
@@ -102,6 +102,10 @@
 #include "presentation-time-server-protocol.h"
 #include "gbm-buffer-backend-server-protocol.h"
 #include "bootkpi/logging.h"
+
+#if defined(RT_SCHEDULE)
+#include "amss/compresmgr_client_api.h"
+#endif
 
 #ifndef DRM_CAP_TIMESTAMP_MONOTONIC
 #define DRM_CAP_TIMESTAMP_MONOTONIC 0x6
@@ -3631,6 +3635,14 @@ drm_backend_create(struct weston_compositor *compositor,
 	int ret;
 	struct weston_head *base, *next;
 
+#if defined(RT_SCHEDULE)
+	CPUConfigReq_t     cpuConfigReq  = {0};
+	CPUConfigResp_t    cpuConfigResp = {0};
+	const char        *procName      = "display";
+	const char        *thrdGrpName   = "compositor-sdm";
+	struct sched_param params;
+#endif
+
 	session_seat = getenv("XDG_SEAT");
 	if (session_seat)
 		seat_id = session_seat;
@@ -3754,6 +3766,26 @@ drm_backend_create(struct weston_compositor *compositor,
 			free(full_init_param);
 			goto err_display;
 		}
+#if defined(RT_SCHEDULE)
+		memset((char *)&params, 0x00, sizeof(struct sched_param));
+		strlcpy(cpuConfigReq.procName, procName, strlen(procName) + 1);
+		strlcpy(cpuConfigReq.thrdGrpName, thrdGrpName, strlen(thrdGrpName) + 1);
+
+		CompResmgrRet_e ret = CompResmgrGetCPUConfig(&cpuConfigReq, &cpuConfigResp);
+
+		if (COMPRESMGR_RET_SUCCESS == ret)
+		{
+			params.sched_priority = cpuConfigResp.priority;
+
+			if (0 != pthread_setname_np(&full_init_tid, cpuConfigReq.thrdGrpName)) {
+				weston_log("pthread_setname_np: %s failed\n", cpuConfigReq.thrdGrpName);
+			} else if (0 != pthread_setschedparam(&full_init_tid, cpuConfigResp.schedPolicy, &params)) {
+				weston_log("pthread_setschedparam: %s failed\n", cpuConfigReq.thrdGrpName);
+			}
+		} else {
+			weston_log("CompResmgrGetCPUConfig: %s failed\n", cpuConfigReq.thrdGrpName);
+		}
+#endif
 	} else {
 		/* If early boot is disabled, do full backend initialization directly. */
 		full_init_main(full_init_param);
