ad52ebd16aa9bf5f9327e5e8e53b152d4876655b
miche <quic_miche@quicinc.com>
2025-03-04 18:46:32 +0800

    weston-sdm-extension: refine compilation rules for RT_SCHEDULE
    1. Arrows the scope of compilation conditions.
    2. Modify the macro definition from RT_SCHEDULE to ENABLE_RT_SCHEDULE to keep the code style uniform
    
    Change-Id: Ifa60e97e36b6b907e6eef30a9355f8bbf62facec
    Signed-off-by: miche <quic_miche@quicinc.com>

--- sdm-backend/compositor-sdm.c
+++ sdm-backend/compositor-sdm.c
@@ -103,7 +103,7 @@
 #include "gbm-buffer-backend-server-protocol.h"
 #include "bootkpi/logging.h"
 
-#if defined(RT_SCHEDULE)
+#if defined(ENABLE_RT_SCHEDULE)
 #include "amss/compresmgr_client_api.h"
 #endif
 
@@ -3635,11 +3635,11 @@ drm_backend_create(struct weston_compositor *compositor,
 	int ret;
 	struct weston_head *base, *next;
 
-#if defined(RT_SCHEDULE)
+#if defined(ENABLE_RT_SCHEDULE)
 	CPUConfigReq_t     cpuConfigReq  = {0};
 	CPUConfigResp_t    cpuConfigResp = {0};
 	const char        *procName      = "display";
-	const char        *thrdGrpName   = "compositor-sdm";
+	const char        *thrdGrpName   = "SDM_compositor";
 	struct sched_param params;
 #endif
 
@@ -3766,7 +3766,7 @@ drm_backend_create(struct weston_compositor *compositor,
 			free(full_init_param);
 			goto err_display;
 		}
-#if defined(RT_SCHEDULE)
+#if defined(ENABLE_RT_SCHEDULE)
 		memset((char *)&params, 0x00, sizeof(struct sched_param));
 		strlcpy(cpuConfigReq.procName, procName, strlen(procName) + 1);
 		strlcpy(cpuConfigReq.thrdGrpName, thrdGrpName, strlen(thrdGrpName) + 1);
@@ -3777,9 +3777,9 @@ drm_backend_create(struct weston_compositor *compositor,
 		{
 			params.sched_priority = cpuConfigResp.priority;
 
-			if (0 != pthread_setname_np(&full_init_tid, cpuConfigReq.thrdGrpName)) {
+			if (0 != pthread_setname_np(full_init_tid, cpuConfigReq.thrdGrpName)) {
 				weston_log("pthread_setname_np: %s failed\n", cpuConfigReq.thrdGrpName);
-			} else if (0 != pthread_setschedparam(&full_init_tid, cpuConfigResp.schedPolicy, &params)) {
+			} else if (0 != pthread_setschedparam(full_init_tid, cpuConfigResp.schedPolicy, &params)) {
 				weston_log("pthread_setschedparam: %s failed\n", cpuConfigReq.thrdGrpName);
 			}
 		} else {
